9c69c9a6e7c92225ce7d2cdf8c3c601a24b2121f
Valentina Popova <solval89@gmail.com>
2024-06-17 12:11:33 +0300

    Exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,41 +1,35 @@
 const Header = (props) => <h1>{props.name}</h1>
 
+const Part = (props) => <p>{props.part.name} {props.part.exercise}</p>
+
 const Content = (props) => {
     return (
         <>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
+            <Part part = {props.parts[0]} />
+            <Part part = {props.parts[1]} />
+            <Part part = {props.parts[2]} />
         </>
     )
 }
 
-const Total = (props) => <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+const Total = (props) => <p>Number of exercises {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}</p>
 
 const App = () => {
     const course = {
         name: 'Half Stack application development',
-        part1: 'Fundamentals of React',
-        exercises1: 10,
-        part2: 'Using props to pass data',
-        exercises2: 7,
-        part3: 'State of a component',
-        exercises3: 14
+        parts: [
+            {name: 'Fundamentals of React', exercise: 10},
+            {name: 'Using props to pass data', exercise: 7},
+            {name: 'State of a component', exercise: 14},
+    ]
     }
 
     return (
-        <div>
+        <>
             <Header name={course.name}/>
-            <Content part1={course.part1} exercises1={course.exercises1} part2={course.part2}
-                     exercises2={course.exercises2} part3={course.part3} exercises3={course.exercises3}/>
-            <Total exercises1={course.exercises1} exercises2={course.exercises2} exercises3={course.exercises3} />
-        </div>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts} />
+        </>
     )
 }
 
