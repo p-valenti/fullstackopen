242fecd875acedd52c04f6c6a7a4c2bb6301d57c
Valentina Popova <solval89@gmail.com>
2024-08-15 17:33:31 +0300

    Exercise 2.15 fixed

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,8 +1,8 @@
 const Persons = ({ shownPersons, deletePerson }) => {
     return (
         <ul>
-            {shownPersons.map(person => (
-                <li key={person.id}>
+            {shownPersons.map((person, index) => (
+                <li key={person.id || index}>
                     {person.name}: {person.number}
                     <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
                 </li>
