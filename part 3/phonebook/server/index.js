12f0d956493235f714aceee9f5a44c74d1cdb36d
Valentina Popova <solval89@gmail.com>
2024-11-29 12:41:46 +0200

    Exercise 3.11

--- part 3/phonebook/server/index.js
+++ part 3/phonebook/server/index.js
@@ -98,6 +98,10 @@ app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`)
