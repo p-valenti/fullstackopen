819084edab1756295eb9317fee41790536908e62
Valentina Popova <solval89@gmail.com>
2025-01-31 12:53:00 +0200

    Exercise 3.21

--- part 3/phonebook/server/index.js
+++ part 3/phonebook/server/index.js
@@ -142,9 +142,9 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+    console.log(`Server running on port ${process.env.PORT || 3001}`);
+});
 
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
 console.log('PORT:', process.env.PORT);
