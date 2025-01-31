b930fa4281d6182ea32269fa0d3c817d067406dd
Valentina Popova <solval89@gmail.com>
2025-01-31 12:47:30 +0200

    Exercise 3.21

--- part 3/phonebook/server/index.js
+++ part 3/phonebook/server/index.js
@@ -141,7 +141,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`)
 })
