a67670992c59412a3eedf2ebb3cdf415132bf0c8
Valentina Popova <solval89@gmail.com>
2025-02-04 12:07:16 +0200

    Exercise 4.2

---
+++ part 4/bloglist/controllers/blogs.js
@@ -0,0 +1,19 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+    try {
+        const blog = new Blog(request.body)
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    } catch (error) {
+        response.status(400).json({ error: error.message })
+    }
+})
+
+module.exports = blogsRouter
--- part 4/bloglist/index.js
+++ part 4/bloglist/index.js
@@ -1,42 +1,8 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err))
-
-app.use(cors())
-app.use(express.json())
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.get('/api/blogs', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-})
-
-app.post('/api/blogs', async (request, response) => {
-    try {
-        const blog = new Blog(request.body)
-        const savedBlog = await blog.save()
-        response.status(201).json(savedBlog)
-    } catch (error) {
-        response.status(400).json({ error: error.message })
-    }
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
