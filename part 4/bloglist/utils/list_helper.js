109179991860c295e1d674a48b3ccdc758b8f86f
Valentina Popova <solval89@gmail.com>
2025-02-12 00:00:36 +0200

    Exercise 4.5

--- part 4/bloglist/utils/list_helper.js
+++ part 4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -6,7 +8,22 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    const blogCount = lodash.countBy(blogs, 'author')
+
+    const topAuthor = lodash.maxBy(Object.keys(blogCount), (author) => blogCount[author])
+
+    return {
+        author: topAuthor,
+        blogs: blogCount[topAuthor]
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostBlogs,
 }
